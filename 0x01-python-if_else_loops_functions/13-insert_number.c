144ac88a609a394d5307939527b0b3f69e219ee4
Mathhmore <math.tadesse@gmail.com>
2023-06-06 14:34:25 +0000

    C function to add a new node into a sorted list

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -27,7 +27,7 @@ listint_t *insert_node(listint_t **head, int number)
 	}
 	else
 	{
-		while (current->next != NULL && current->next->n < number)
+		while (current->next != NULL && current->next->n <= number)
 		{
 			current = current->next;
 		}
