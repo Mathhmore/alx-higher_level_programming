808edfdc02d6bb3ab9bf7733be53a06b74021b35
root <math.tadesse@gmail.com>
2023-06-08 03:22:55 -0700

    Importing add function from add_0.py file

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -3,4 +3,4 @@ if __name__ = "__main__":
     import add from add _0
     a = 1
     b = 2
-    print("{} + {} = {}".format(a, b, add(a, b)))
+    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
