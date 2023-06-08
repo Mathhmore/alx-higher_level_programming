e3002a5219911c34db6d8613140d172bfd017923
root <math.tadesse@gmail.com>
2023-06-08 05:20:06 -0700

    program to do a simple operation of the arguments

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,21 +1,23 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    from calculator_1 import add, sub, mul, div
     from sys import argv
+    from calculator_1 import add, sub, mul, div
+    a = argv[1]
+    op = argv[2]
+    b = argv[3]
     result = 0
-    if len(argv) - 1 != 3:
+    if len(argv[1:]) != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
-    if argv[2] != '+' or argv[2] != '-' or argv[2] != '*' or argv[2] != '/':
+    if op == '+':
+        result = add(int(argv[1]), int(argv[3]))
+    elif op == '-':
+        result = sub(int(argv[1]), int(argv[3]))
+    elif op == '*':
+        result = mul(int(argv[1]), int(argv[3]))
+    elif op == '/':
+        result = div(int(argv[1]), int(argv[3]))
+    else:
         print("Unknown operator. Available operators: +, -, * and /")
         exit(1)
-    else:
-        if argv[2] == '+':
-            result = add(int(argv[1]), int(argv[3]))
-        elif argv[2] == '-':
-            result = sub(int(argv[1]), int(argv[3]))
-        elif argv[2] == '*':
-            result = mul(int(argv[1]), int(argv[3]))
-        elif argv[2] == '/':
-            result = div(int(argv[1]), int(argv[3]))
-        print("{:d} {} {:d} = {:d}".format(argv[1], argv[2], argv[3], result))
+    print("{} {} {} = {:d}".format(argv[1], argv[2], argv[3], result))
