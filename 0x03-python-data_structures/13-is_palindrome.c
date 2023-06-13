207abe78715d2839324c3968df24396c3b61c5af
root <math.tadesse@gmail.com>
2023-06-12 21:28:07 -0700

    Palindrome or not singly linked list

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,6 +1,22 @@
 #include "lists.h"
 
 /**
+ * count - counts number of elements in a singly linked list
+ * @cm: list
+ * Retern: count
+ */
+int count(listint_t *cm)
+{
+	int co;
+
+	while (cm != NULL)
+	{
+		co++;
+		cm = cm->next;
+	}
+	return (co);
+}
+/**
  *is_palindrome - check whether a given singly linked list is palindrome or not
  * @head: head of the list
  * Return: 0 if it is not a palindrome, 1 if it is a palindrome
@@ -9,19 +25,18 @@ int is_palindrome(listint_t **head)
 {
 	if (*head == NULL)
 		return (1);
-	listint_t *temp, *h, *cm;
+	listint_t *temp;
+	listint_t *h;
+	listint_t *cm;
 
 	temp = *head;
 	h = *head;
 	cm = *head;
-	int i = 0, counter = 0, c, c1;
+	int i = 0;
+	int counter = count(cm);
+	int c;
+	int c1;
 
-	while (cm != NULL)
-	{
-		counter++;
-		cm = cm->next;
-	}
-	printf("%d", counter);
 	c = counter - 1;
 	c1 = c * c;
 	while (temp->next != NULL)
