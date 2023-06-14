2e3293f76a502a292c0dc4b4ac5ce32dc452d5fb
root <math.tadesse@gmail.com>
2023-06-14 11:47:54 -0700

    Find the weighted average

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+    n = 0
+    m = 1
+    d = 0
+    c = 0
+    for i in list(my_list):
+        for j in i:
+            c += 1
+            m *= j
+            if c == len(i):
+                k = j
+        d += k
+        n += m
+        c = 0
+        m = 1
+    return n / d
