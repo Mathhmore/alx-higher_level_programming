12c504b9b88e15cd48dc5d3b146c179a35c10f3c
root <math.tadesse@gmail.com>
2023-06-14 05:04:34 -0700

    function to Square each elements of a given matrix and store in a new matrix

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
     k = 0
-    l = 0
-    new_matrix = [[0 for _ in range(len(matrix[0]))] for _ in range(len(matrix))]
+    m = 0
+    col = len(matrix[0])
+    row = len(matrix)
+    new_matrix = [[0 for _ in range(col)] for _ in range(row)]
     for i in matrix:
         for j in i:
             new_matrix[k][l] = j * j
-            l += 1
+            m += 1
         k += 1
-        l = 0
+        m = 0
     return new_matrix
