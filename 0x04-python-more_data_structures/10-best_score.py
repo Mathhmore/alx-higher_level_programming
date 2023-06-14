b6857bc757977db73ff993f8a9308391da773f2c
root <math.tadesse@gmail.com>
2023-06-14 09:44:31 -0700

    Find name with the highest score from the dictionary

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,9 +2,10 @@
 def best_score(a_dictionary):
     mx = 0
     name = ""
-    if a_dictionary:
-        for i in a_dictionary:
-            if a_dictionary[i] > mx:
-                mx = a_dictionary[i]
-                name = i
+    if a_dictionary is None:
+        return None
+    for i in a_dictionary:
+        if a_dictionary[i] > mx:
+            mx = a_dictionary[i]
+            name = i
     return name
