3406292017c9e4bf52b6178d7da1d7fa8dc83750
root <math.tadesse@gmail.com>
2023-06-14 08:16:18 -0700

    Find differences of the given two sets

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
 def only_diff_elements(set_1, set_2):
     new_set = []
-    c = 0
     for i in set_1:
         if i not in set(set_2):
             new_set.append(i)
-    c = 0
     for k in set_2:
         if k not in set(set_1):
             new_set.append(k)
