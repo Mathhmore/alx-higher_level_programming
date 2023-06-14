0b127b3419bd3db44f293fbdf284fa2ece6af9f6
root <math.tadesse@gmail.com>
2023-06-14 12:26:13 -0700

    Delete a key with a given specific value

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def complex_delete(a_dictionary, value):
-    for i in a_dictionary:
+    for i in list(a_dictionary):
         if a_dictionary[i] == value:
-            del i
+            a_dictionary.pop(i)
     return a_dictionary
