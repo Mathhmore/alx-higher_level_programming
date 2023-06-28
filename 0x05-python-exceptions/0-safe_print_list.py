76a995d4fabf67a1c1c23a330c4490cb6e3d1ee4
root <math.tadesse@gmail.com>
2023-06-28 04:08:56 -0700

    Print elements of an array using try and except

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    c = 0
+    try:
+        for i in range(0, x):
+            print(my_list[i], end="")
+            c += 1
+        print()
+        return c
+    except:
+        print()
+        return c
