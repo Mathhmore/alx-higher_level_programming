027d4042882ad38714dc61534383816e4989329f
root <math.tadesse@gmail.com>
2023-06-28 11:11:06 -0700

    handle va;ue and type error for integer and write the error on stderr

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def safe_print_integer(value):
+    try:
+        print("{:d}".format(value))
+        return True
+    except ValueError as err:
+        stderr.write("Exception: {}\n".format(err))
+        return False
+    except TypeError as err:
+        stderr.write("Exception: {}\n".format(err))
+        return False
