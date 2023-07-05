b3b725ad536927ee1cd923b00fd1eeaa1688830e
root <math.tadesse@gmail.com>
2023-07-05 09:50:35 -0700

    function print my name

--- 0x07-python-test_driven_development/m.py
+++ 0x07-python-test_driven_development/m.py
@@ -1,9 +1,4 @@
 #!/usr/bin/python3
-matrix_divided = __import__('2-matrix_divided').matrix_divided
+say_my_name = __import__('3-say_my_name').say_my_name
 
-matrix = [
-    [1, 2, 3],
-    [4, 5, 6]
-]
-print(matrix_divided(None, None))
-print(matrix)
+say_my_name(None, "Bekele")
