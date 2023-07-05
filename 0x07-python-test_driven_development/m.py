50753a71fc18a4b87a0958db945930c7b8255930
root <math.tadesse@gmail.com>
2023-07-05 10:28:23 -0700

    function to print a square

--- 0x07-python-test_driven_development/m.py
+++ 0x07-python-test_driven_development/m.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python3
-say_my_name = __import__('3-say_my_name').say_my_name
+print_square = __import__('4-print_square').print_square
 
-say_my_name(None)
+print_square(4)
+print("")
+print_square(10)
+print("")
+print_square(0)
+print("")
+print_square(1)
